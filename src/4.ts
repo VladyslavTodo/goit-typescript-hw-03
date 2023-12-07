317dba71cae1818f4c7f988b48fdff0a94064e09
VladyslavTodo <124445034+VladyslavTodo@users.noreply.github.com>
2023-12-07 22:58:36 +0200

    i hope finished

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,57 @@
+class Key {
+    private signature: number;
+
+    constructor() {
+        this.signature = Math.random();
+    }
+
+    getSignature(): number {
+        return this.signature;
+    }
+}
+
+class Person {
+    private key: Key;
+
+    constructor(key: Key) {
+        this.key = key;
+    }
+
+    getKey(): Key {
+        return this.key;
+    }
+}
+
+abstract class House {
+    door: boolean;
+    key: Key;
+    tenants: Person[] = [];
+
+    constructor(key: Key) {
+        this.door = false;
+        this.key = key;
+    }
+
+    abstract openDoor(key: Key): void;
+
+    comeIn(person: Person): void {
+        if (!this.door) return;
+        this.tenants.push(person);
+    }
+}
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (this.key.getSignature() === key.getSignature()) {
+            console.log("Open");
+
+            this.door = true;
+        } else {
+            console.log("Close");
+        }
+    }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +60,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
